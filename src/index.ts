86acc67bdf9499ceb4f8e97acc85cea27739b52c
Sam Vuckovic <42130427+shock59@users.noreply.github.com>
2026-06-16 18:55:35 +1000

    Add basic options to API

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,26 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import mbgl from "@maplibre/maplibre-gl-native";
 import sharp from "sharp";
+import * as zod from "zod";
+
+const MapOptions = zod.object({
+  width: zod.coerce.number().min(1).max(2000).default(640),
+  height: zod.coerce.number().min(1).max(2000).default(480),
+  lat: zod.coerce.number().min(-90).max(90),
+  lon: zod.coerce.number(),
+  zoom: zod.coerce.number().min(0).max(21).default(0),
+});
 
 const res = await fetch("https://tiles.samv.me/style/style");
 const style = await res.json();
 
 const map = new mbgl.Map();
 map.load(style);
-const render: () => Promise<Uint8Array<ArrayBufferLike>> = () => {
+const render: (
+  options: mbgl.RenderOptions,
+) => Promise<Uint8Array<ArrayBufferLike>> = (options) => {
   return new Promise((resolve, reject) => {
-    map.render((err, buffer) => {
+    map.render(options, (err, buffer) => {
       if (err) {
         reject(err);
         return;
@@ -24,12 +35,26 @@ const app = new Hono();
 app.get("/", (c) => c.text("hai"));
 
 app.get("/map", async (c) => {
-  const buffer = await render();
+  const { error, data: options } = MapOptions.safeParse(c.req.query());
+  if (error) {
+    const issue = error.issues[0];
+    return c.text(
+      issue ? `${String(issue.path[0])}: ${issue.message}` : "Error",
+      400,
+    );
+  }
+
+  const buffer = await render({
+    width: options.width,
+    height: options.height,
+    center: [options.lon, options.lat],
+    zoom: options.zoom,
+  });
 
   const image = await sharp(buffer, {
     raw: {
-      width: 512,
-      height: 512,
+      width: options.width,
+      height: options.height,
       channels: 4,
     },
   })
